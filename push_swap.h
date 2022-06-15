1f823287d159515d5f434bf7103bf84edb508bd2
Soraya Lepetit <slepetit@1H2.42angouleme.fr>
2022-06-15 03:21:36 +0200

    parse arg is ok

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slepetit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 17:55:59 by slepetit          #+#    #+#             */
+/*   Updated: 2022/06/15 03:15:55 by slepetit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <stddef.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_error();
+int	ft_space_neg(char *stack);
+int	ft_check(char *stack);
+
+#endif
